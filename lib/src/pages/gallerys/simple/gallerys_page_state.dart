ff8ede2da9d871162eb038b7a148345f4bcdd664
errorfg <qq1504106973@gmail.com>
2026-02-13 20:40:16 +0800

    fix: enable desktop home sync via title with progress

--- lib/src/pages/gallerys/simple/gallerys_page_state.dart
+++ lib/src/pages/gallerys/simple/gallerys_page_state.dart
@@ -2,6 +2,9 @@ import '../../../routes/routes.dart';
 import '../../base/base_page_state.dart';
 
 class GallerysPageState extends BasePageState {
+  bool syncInProgress = false;
+  double syncProgress = 0;
+
   @override
   String get route => Routes.gallerys;
 }
